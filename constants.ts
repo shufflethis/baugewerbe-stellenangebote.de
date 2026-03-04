3355af8de6d9ffce20f56f34724bdb1074abe8a0
Autoblogger <dev@famefact.de>
2026-03-04 13:33:16 +0100

    chore: update job listings from external sources (HR-JobScraper)
    25 jobs injected from: Arbeitsagentur, Indeed, StepStone, meinestadt.de

--- constants.ts
+++ constants.ts
@@ -3,29 +3,202 @@ import { JobListing } from './types';
 export const MOCK_JOBS: JobListing[] = [
   {
     id: 1,
-    title: 'Bauleiter Hochbau (m/w/d)',
-    company: 'Müller Bau GmbH',
-    location: 'Berlin',
+    title: 'Kundenberater (m/w/d)',
+    company: 'Boerger Tankservice GmbHSofort-Bewerbung',
+    location: 'München',
     type: 'Vollzeit',
-    salary: '4.200€ - 5.500€',
-    postedAt: 'Vor 2 Tagen'
+    postedAt: '2026-03-04'
   },
   {
     id: 2,
-    title: 'Maurer (m/w/d)',
-    company: 'Schmidt & Partner Bau',
-    location: 'München',
+    title: 'Fahrer und Messgehilfe (m/w/d)',
+    company: 'Landratsamt Schwäbisch Hall',
+    location: 'Crailsheim',
     type: 'Vollzeit',
-    salary: '3.200€ - 3.800€',
-    postedAt: 'Neu'
+    postedAt: '2026-03-04'
   },
   {
     id: 3,
-    title: 'Zimmerer / Zimmermann (m/w/d)',
-    company: 'HolzBau Weber GmbH',
-    location: 'Hamburg',
+    title: 'Fahrer und Messgehilfe (m/w/d)',
+    company: 'Landratsamt Schwäbisch Hall',
+    location: 'Schwäbisch Hall',
     type: 'Vollzeit',
-    salary: '3.400€ - 4.000€',
-    postedAt: 'Vor 1 Woche'
+    postedAt: '2026-03-04'
+  },
+  {
+    id: 4,
+    title: 'Hauswirtschaftliche Leitung (m/w/d) für unser Kinderhaus in Teil- oder Vollzeit',
+    company: 'Kinder- und Jugendhilfe Weidenkorb',
+    location: 'Löhne',
+    type: 'Vollzeit',
+    postedAt: '2026-03-04'
+  },
+  {
+    id: 5,
+    title: 'Hauswirtschaftliche Leitung (m/w/d) für unser Kinderhaus in Teil- oder Vollzeit',
+    company: 'Kinder- und Jugendhilfe Weidenkorb',
+    location: 'Espelkamp',
+    type: 'Vollzeit',
+    postedAt: '2026-03-04'
+  },
+  {
+    id: 6,
+    title: 'Physiotherapeut*in (m/w/d) mit Zusatzqualifikation',
+    company: 'Reha-Zentrum Gesundheit Nordhessen GmbH',
+    location: 'Kassel',
+    type: 'Vollzeit',
+    postedAt: '2026-03-04'
+  },
+  {
+    id: 7,
+    title: 'Lagerist (m/w/d) im Baugewerbe',
+    company: 'Balthasar Höhn Bauunternehmung GmbH & Co.KG',
+    location: 'Würzburg',
+    type: 'Vollzeit',
+    postedAt: '2026-03-02'
+  },
+  {
+    id: 8,
+    title: 'Bürokaufmann (m/w/d) im Baugewerbe',
+    company: 'spp direkt Frankfurt GmbH Niederlassung Offenbach',
+    location: 'Mühlheim am Main',
+    type: 'Vollzeit',
+    postedAt: '2026-03-02'
+  },
+  {
+    id: 9,
+    title: 'LKW-Fahrer/in im Baugewerbe (m/w/d)',
+    company: 'WF Tief- und Rohrbau GmbH & Co. KG',
+    location: 'Ebermannstadt',
+    type: 'Vollzeit',
+    postedAt: '2026-02-27'
+  },
+  {
+    id: 10,
+    title: 'Reinigungskraft - Baugewerbe - Charlottenburg (m/w/d)',
+    company: 'PerZukunft Arbeitsvermittlung GmbH&Co.KG',
+    location: 'Berlin',
+    type: 'Vollzeit',
+    postedAt: '2026-02-19'
+  },
+  {
+    id: 11,
+    title: 'Kalkulator/in für die Angebotserstellung (Baugewerbe)',
+    company: 'Malermeister Pluhm & Zankel GmbH',
+    location: 'Hanau',
+    type: 'Vollzeit',
+    postedAt: '2026-02-18'
+  },
+  {
+    id: 12,
+    title: 'Handwerker/in im Baugewerbe (w/m/d)',
+    company: 'Niedersachsen Ports GmbH & Co. KG',
+    location: 'Cuxhaven',
+    type: 'Vollzeit',
+    postedAt: '2026-02-17'
+  },
+  {
+    id: 13,
+    title: 'Baugewerbe',
+    company: 'Maurermeister Sven Rudow GmbH',
+    location: 'Nennhausen',
+    type: 'Vollzeit',
+    postedAt: '2026-02-15'
+  },
+  {
+    id: 14,
+    title: 'Fachkraft (m/w/d) - Baugewerbe',
+    company: 'Fabian Lodes',
+    location: 'Ebermannstadt',
+    type: 'Vollzeit',
+    postedAt: '2026-02-13'
+  },
+  {
+    id: 15,
+    title: 'Projektmanager (m/w/d) im Baugewerbe',
+    company: 'EMshield GmbH Technopark I',
+    location: 'Grasbrunn',
+    type: 'Vollzeit',
+    postedAt: '2026-02-12'
+  },
+  {
+    id: 16,
+    title: 'Helfer Baugewerbe (m/w/d)',
+    company: 'Roof-Tec Dachdeckerei GmbH & Co. KG',
+    location: 'Taarstedt',
+    type: 'Vollzeit',
+    postedAt: '2026-02-10'
+  },
+  {
+    id: 17,
+    title: 'Kaufmännischer Mitarbeiter - Baugewerbe (M/W/D)',
+    company: 'BESTpro Personalkonzepte GmbH',
+    location: 'Gevelsberg',
+    type: 'Vollzeit',
+    postedAt: '2026-02-04'
+  },
+  {
+    id: 18,
+    title: 'Mitarbeiter/in im Baugewerbe (m/w/d)',
+    company: 'Vogler Bau GmbH',
+    location: 'Unstruttal',
+    type: 'Vollzeit',
+    postedAt: '2026-02-03'
+  },
+  {
+    id: 19,
+    title: 'LKW-Fahrer (m/w/d) im Baugewerbe',
+    company: 'Hauck Betriebs-GmbH',
+    location: 'Waibstadt',
+    type: 'Vollzeit',
+    postedAt: '2026-01-22'
+  },
+  {
+    id: 20,
+    title: 'Mitarbeiter/in im Baugewerbe (m/w/d)',
+    company: 'Heeger Bau',
+    location: 'Unstruttal',
+    type: 'Vollzeit',
+    postedAt: '2026-01-15'
+  },
+  {
+    id: 21,
+    title: 'Projektleiter im Baugewerbe (m/w/d)',
+    company: 'EMPORACON Holding GmbH',
+    location: 'Krefeld',
+    type: 'Vollzeit',
+    postedAt: '2026-01-13'
+  },
+  {
+    id: 22,
+    title: 'Hilfsarbeiter (m/w/d) im Baugewerbe',
+    company: 'Bauunternehmung Stefan Schmitt',
+    location: 'Ochtendung',
+    type: 'Vollzeit',
+    postedAt: '2025-11-17'
+  },
+  {
+    id: 23,
+    title: 'Allrounder im Baugewerbe gesucht (m/w/d)',
+    company: 'Tim Petersen',
+    location: 'Breiholz',
+    type: 'Vollzeit',
+    postedAt: '2025-10-28'
+  },
+  {
+    id: 24,
+    title: 'Lkw-Fahrer (m/w/d) im Baugewerbe',
+    company: 'Brigitte Fischer',
+    location: 'Nürtingen',
+    type: 'Vollzeit',
+    postedAt: '2025-10-24'
+  },
+  {
+    id: 25,
+    title: 'Facharbeiter (m/w/d) im Baugewerbe',
+    company: 'Dröder Spedition GmbH &Co.KG',
+    location: 'Künzell',
+    type: 'Vollzeit',
+    postedAt: '2025-09-30'
   }
 ];
